736342d1bdc82396328c6c90cb1f5ce040c54680
Michele_Di_Mezza_Cutillo <micheleilpro@libero.it>
2024-07-12 19:18:41 +0200

    aiuto

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,7 +3,11 @@ package com;
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< Upstream, based on origin/main
 		System.out.println("Vaffanculo Mike.");
+=======
+		System.out.println("Ciao mondo");
+>>>>>>> 49a1bdd Modificato messaggio
 	}
 
 }
