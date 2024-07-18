13d7c84c29af028a4be1f88c53996a7598bf3f69
roj0II <rocco.santorsa.01@gmail.com>
2024-07-18 16:49:16 +0200

    Sgozzo Michele

---
+++ src/main/java/com/gamesgo/GamesGoApplication.java
@@ -0,0 +1,13 @@
+package com.gamesgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesGoApplication.class, args);
+	}
+
+}
