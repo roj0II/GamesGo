7b0acc82759f95f8c3a5e2f10818944475c9d44f
roj0II <rocco.santorsa.01@gmail.com>
2024-07-19 17:16:01 +0200

    db delete

--- src/main/java/com/gamesgo/model/Gamegenre.java
+++ src/main/java/com/gamesgo/model/Gamegenre.java
@@ -4,6 +4,7 @@ package com.gamesgo.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
@@ -12,6 +13,8 @@ import jakarta.persistence.ManyToOne;
 public class Gamegenre implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	private int id;
 	@ManyToOne
 	@JoinColumn(name = "idGame")
 	private Game game;
