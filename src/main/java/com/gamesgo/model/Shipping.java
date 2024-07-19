fa1d55b32a30b2e3a35b2c071f430f244a921bda
DESKTOP-EDL9RSN\andrea <gold_roger95@hotmail.it>
2024-07-19 17:13:00 +0200

    Aggiunte 4 entità

--- src/main/java/com/gamesgo/model/Shipping.java
+++ src/main/java/com/gamesgo/model/Shipping.java
@@ -1,8 +1,25 @@
 package com.gamesgo.model;
 
+import java.io.Serializable;
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Shipping {
-
+public class Shipping implements Serializable {
+	@Id
+	private int id;
+	@ManyToOne
+	@JoinColumn(name="idTransaction")
+	private int idTransaction;
+	@ManyToOne
+	@JoinColumn(name="idUser")
+	private int idUser;
+	Date shippingDate;
+	Date scheduleDeliveryDate;
+	private String status;
+	
 }
--- src/main/java/com/gamesgo/model/Storage.java
+++ src/main/java/com/gamesgo/model/Storage.java
@@ -1,8 +1,19 @@
 package com.gamesgo.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Storage {
-
+public class Storage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @ManyToOne
+    @JoinColumn(name="idGame")
+    private Game game;
+    @Id
+    private int id;
+    private int amount;
 }
