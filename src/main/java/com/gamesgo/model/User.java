0ab15f7b1c02873eb7b1178be4cd96d5085ccb7d
DESKTOP-EDL9RSN\andrea <gold_roger95@hotmail.it>
2024-07-19 09:45:34 +0200

    Aggiunti campi classe User

--- src/main/java/com/gamesgo/model/User.java
+++ src/main/java/com/gamesgo/model/User.java
@@ -1,8 +1,23 @@
 package com.gamesgo.model;
 
+import java.io.Serializable;
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
-public class User {
-
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    private int id;
+    @Column
+    private String username,name, surname;
+    @Column
+    private String address, phone, email, password;
+    @Column
+    private boolean admin;
+    
+    
 }
