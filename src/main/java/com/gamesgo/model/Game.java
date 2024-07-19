715ad77462d9e218ebb622923524a9303d318263
alessio399 <alessiomarino45@gmail.com>
2024-07-19 09:41:14 +0200

    Dichiarate le variabili dell'entità 'Game'.

--- src/main/java/com/gamesgo/model/Game.java
+++ src/main/java/com/gamesgo/model/Game.java
@@ -1,8 +1,24 @@
 package com.gamesgo.model;
 
+import java.sql.Date;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Game {
-
+	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	private int id;
+	
+	private String title;
+	private String author;
+	private Date releaseDate;
+	private String description;
+	private String photoUrl;
+	private float price;
+	private float downloadPrice;
+	private String type;
+	
+	
 }
