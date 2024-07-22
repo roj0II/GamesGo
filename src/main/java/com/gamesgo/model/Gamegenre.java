41a1f31897f2ada88f65cf758dc5fb797c150160
alessio399 <alessiomarino45@gmail.com>
2024-07-22 09:08:50 +0200

    Completata l'Entity Rent e aggiustate le altre (prime 4)

--- src/main/java/com/gamesgo/model/Gamegenre.java
+++ src/main/java/com/gamesgo/model/Gamegenre.java
@@ -6,19 +6,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-
 @Entity
 public class Gamegenre implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	@Id
 	private int id;
 
 	@ManyToOne
 	@JoinColumn(name = "idGame")
 	private Game game;
-		
+
 	@ManyToOne
 	@JoinColumn(name = "idGenre")
 	private Genre genre;
