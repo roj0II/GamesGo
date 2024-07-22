7393d162fb162f227f585fb4826f7bdd608cb86d
MikeDmC <micheleilpro@libero.it>
2024-07-22 15:48:27 +0200

    aggiunto un messaggio nel home controller

--- src/main/java/com/gamesgo/controller/HomeController.java
+++ src/main/java/com/gamesgo/controller/HomeController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HomeController {
 
-	@GetMapping("/login")
+	@GetMapping("/")
 	public String login () {
-		return "loginPage.html";
+		return "index.jsp";
 	}
 	
 }
