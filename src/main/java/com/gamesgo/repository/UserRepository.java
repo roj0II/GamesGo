35203b50351c02f20a88706a3ea1021c903ee2f8
roj0II <rocco.santorsa.01@gmail.com>
2024-07-26 16:36:13 +0200

    .

--- src/main/java/com/gamesgo/repository/UserRepository.java
+++ src/main/java/com/gamesgo/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.gamesgo.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gamesgo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+    
+    
 }
