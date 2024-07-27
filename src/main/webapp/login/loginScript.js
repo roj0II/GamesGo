dce53e5a8f012beb6607d9972b56e9d3964ac146
roj0II <rocco.santorsa.01@gmail.com>
2024-07-27 02:19:13 +0200

    Cambiata la reference nello script del login

--- src/main/webapp/login/loginScript.js
+++ src/main/webapp/login/loginScript.js
@@ -20,7 +20,7 @@ function validateInput() {
 
 	if (inputValue.toLowerCase().includes(targetWord)) {
 		submitButton.innerHTML = "Accedi Admin";
-		submitButton.onclick = function() { window.location.href = 'admin.html'; };
+		submitButton.onclick = function() { window.location.href = 'admin'; };
 	} else {
 		submitButton.innerHTML = "Accedi Utente";
 		submitButton.onclick = function() { window.location.href = 'user.jsp'; };
