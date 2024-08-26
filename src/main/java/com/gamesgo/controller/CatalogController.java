0dae647627fd4a305faaca363d7bd79dc1b98c30
alessio399 <alessiomarino45@gmail.com>
2024-08-26 17:12:51 +0200

    CatalogController.java aggiornato con i popups

--- src/main/java/com/gamesgo/controller/CatalogController.java
+++ src/main/java/com/gamesgo/controller/CatalogController.java
@@ -16,18 +16,36 @@ public class CatalogController {
 	@Autowired
 	private GenreRepository genRep;
 
-	final private String catalogPath = "catalog.jsp";
+	final private static String catalogPath = "catalog.jsp";
 
 	@GetMapping("/catalog")
 	public String catalog(Model model) {
-		model.addAttribute("games", gameRep.findAll());
+		getDefaultCatalog(model);	
 		return catalogPath;
 	}
 
 	@PostMapping("/catalog")
 	public String catalogFilter(@RequestParam String opType, @RequestParam String input, Model model) {
-		if (opType == null)
-			System.out.println("ciaone");
+		if (opType == null) {
+			model.addAttribute("show", "show");
+			model.addAttribute("message", "Operazione nulla");
+			model.addAttribute("color", "red");
+			model.addAttribute("title", "Errore!");
+			getDefaultCatalog(model);
+
+			return catalogPath;
+		}
+		
+		if(input == null) {
+			model.addAttribute("show", "show");
+			model.addAttribute("message", "Input nullo");
+			model.addAttribute("color", "red");
+			model.addAttribute("title", "Errore!");
+			getDefaultCatalog(model);
+
+			return catalogPath;
+		}
+
 		switch (opType) {
 		case "":
 			break;
@@ -36,9 +54,20 @@ public class CatalogController {
 		case "ciaone":
 			break;
 		default:
-			
+			model.addAttribute("show", "show");
+			model.addAttribute("message", "Operazione non riconosciuta");
+			model.addAttribute("color", "red");
+			model.addAttribute("title", "Errore!");
+			getDefaultCatalog(model);
+
 			break;
 		}
+		
 		return catalogPath;
+	}
+
+	public void getDefaultCatalog(Model model) {
+		model.addAttribute("games", gameRep.findAll());
+		model.addAttribute("genres", genRep.findAll());
 	}
 }
