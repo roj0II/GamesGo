5f299fa79245c05133e05c1c5ad2c8fd5ce136a9
roj0II <rocco.santorsa.01@gmail.com>
2024-09-01 04:06:44 +0200

    Fixata interfaccia CrudController
    Mancava HttpSession session nei parametri delle firme dei metodi.
    Sgozzo Michele. Baci stellari.

--- src/main/java/com/gamesgo/interfaces/CrudControllerI.java
+++ src/main/java/com/gamesgo/interfaces/CrudControllerI.java
@@ -3,16 +3,18 @@ package com.gamesgo.interfaces;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.servlet.http.HttpSession;
+
 /**
  * {@code CrudControllerI} serve a dare un'impronta ai controller che gestiranno direttamente l'entità.
  * @param Dto intendiamo il Dto dell'entità che stiamo andando ad usare.
  * @author Michele
  */
 public interface CrudControllerI<Dto> {
-	String main(Model model);
-	String preInsert(Model model);
-	String insert(Model model, Dto dto);
-	String preUpdate(Model model, @PathVariable int id);
-	String update(Model model, Dto dto);
-	String delete(Model model, @PathVariable int id);
+	String main(Model model, HttpSession session);
+	String preInsert(Model model, HttpSession session);
+	String insert(Model model, HttpSession session, Dto dto);
+	String preUpdate(Model model, HttpSession session, @PathVariable int id);
+	String update(Model model, HttpSession session, Dto dto);
+	String delete(Model model, HttpSession session, @PathVariable int id);
 }
