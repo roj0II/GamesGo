94684898c55284a72b89ad9f55b0d7d4b5bf245c
MikeDmC <micheleilpro@libero.it>
2024-09-03 15:54:31 +0200

    aggiunto l'endpoint post per ricevere l'evento del bottone premuto

--- src/main/java/com/gamesgo/controller/HomeController.java
+++ src/main/java/com/gamesgo/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.gamesgo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gamesgo.util.WebhookManager;
 
 @Controller
 public class HomeController {
@@ -15,5 +21,20 @@ public class HomeController {
 	public String contact () {
 		return "home/contact.html";
 	}
+	
+	@PostMapping("/contact")
+	public String contactUs (Model model,
+			@RequestParam("name") String name,
+			@RequestParam("surname") String surname,
+			@RequestParam("email") String email,
+			@RequestParam("subject") String subject,
+			@RequestParam("message") String message
+	) throws Exception {
+		
+		WebhookManager.sendWebhook("https://discord.com/api/webhooks/1280482865393569843/o-PwN4IXGJzw8B-dM6ppPOnsI3PAlsmE4mGH8GOhj1FenH5nUPZ9AKmsct1Y3uCPjLmX", WebhookManager.embedContact(name, surname, subject, email, message));
+
+        return "redirect:/contact";
+	}
+	
 	
 }
