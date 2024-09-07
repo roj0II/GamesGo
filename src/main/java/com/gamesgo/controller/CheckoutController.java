9c4d7bb592d5a9ec48346173a30d2c24cb440849
MikeDmC <micheleilpro@libero.it>
2024-09-07 20:58:01 +0200

    Iniziati i lavori per il backend di checkout

--- src/main/java/com/gamesgo/controller/CheckoutController.java
+++ src/main/java/com/gamesgo/controller/CheckoutController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gamesgo.dto.CheckoutDto;
+import com.gamesgo.dto.GameDto;
 import com.gamesgo.model.Game;
 import com.gamesgo.model.User;
 import com.gamesgo.repository.GameRepository;
@@ -40,6 +42,20 @@ public class CheckoutController {
 		}
 		
 		CheckoutDto cd = new CheckoutDto();
+		cd.setTransactionAddress(loggedUser.getAddress());
+		cd.setTransactionEmail(loggedUser.getEmail());
+		cd.setTransactionName(loggedUser.getName());
+		cd.setTransactionPhone(loggedUser.getPhone());
+		cd.setTransactionSurname(loggedUser.getSurname());
+		
+		cd.setShippingAddress(loggedUser.getAddress());
+		cd.setShippingEmail(loggedUser.getEmail());
+		cd.setShippingName(loggedUser.getName());
+		cd.setShippingPhone(loggedUser.getPhone());
+		cd.setShippingSurname(loggedUser.getSurname());
+		
+		cd.setPaymentNameAndSurname(loggedUser.getName()+ " "+loggedUser.getSurname());
+		
 		cd.setUserAddress(loggedUser.getAddress());
 		cd.setUserEmail(loggedUser.getEmail());
 		cd.setUserName(loggedUser.getName());
@@ -53,12 +69,44 @@ public class CheckoutController {
 		} else {
 			cd.setGamePrice(game.getPriceRetail());
 		}
-		cd.setOnline(formatType.equals("online"));
+		
+		if (transactionType.equals("rent")) {
+			cd.setGamePrice((float) (cd.getGamePrice()/30 - 0.20));
+		}
+		
+		cd.setOnline(formatType.equals("digital"));
 		cd.setRent(transactionType.equals("rent"));
 		
 		
 		
 		model.addAttribute("check", cd);
 		return pathCheckoutPage;
+	}
+	
+	@PostMapping("/final")
+	public String checkout(Model model, HttpSession session, @ModelAttribute("check") CheckoutDto checkoutDto){
+		
+		// controlliamo se è un buy o rent.
+		if (checkoutDto.isRent()) { // rent.
+			
+			if (checkoutDto.isOnline()) { // online.
+				
+				
+				
+			} else { // fisico.
+				
+			}
+			
+		} else { // buy.
+			
+			if (checkoutDto.isOnline()) { // online.
+				
+			} else { // fisico.
+				
+			}
+			
+		}
+		
+		return "";
 	}
 }
