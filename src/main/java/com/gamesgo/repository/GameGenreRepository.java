72d92f4b22e969e564ac53b0cdff5d4dee6cafd3
alessio399 <alessiomarino45@gmail.com>
2024-09-09 18:15:04 +0200

    Aggiunto controllo ad inserimento e aggiornamento Game-Genre

--- src/main/java/com/gamesgo/repository/GameGenreRepository.java
+++ src/main/java/com/gamesgo/repository/GameGenreRepository.java
@@ -1,12 +1,11 @@
 package com.gamesgo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+import com.gamesgo.model.Game;
 import com.gamesgo.model.Gamegenre;
+import com.gamesgo.model.Genre;
 
 public interface GameGenreRepository extends JpaRepository<Gamegenre, Integer> {
-
+	public Gamegenre findByGameAndGenre(Game game, Genre genre);
 }
