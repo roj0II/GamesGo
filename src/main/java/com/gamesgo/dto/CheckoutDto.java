f2d43aac1e425b6131f6f6ba898520c706c1499b
MikeDmC <micheleilpro@libero.it>
2024-09-09 22:41:03 +0200

    modificato il model dto

--- src/main/java/com/gamesgo/dto/CheckoutDto.java
+++ src/main/java/com/gamesgo/dto/CheckoutDto.java
@@ -8,12 +8,6 @@ public class CheckoutDto {
 	private boolean online;
 	private boolean rent; 
 	
-	// Dati utente, Ottenuti dal loggedUser.
-	private String userAddress;
-	private String userEmail;
-	private String userPhone;
-	private String userName;
-	private String userSurname;
 	// private boolean admin; se vogliamo, facciamo che all'admin si applica 20% di sconto automaticamente.EXTRA
 	
 	// Dati gioco, Ottenuti dal gioco selezionato per l'acquisto/affitto.
@@ -52,36 +46,6 @@ public class CheckoutDto {
 	private String transactionEmail;
 	// i soldi che paga sarà calcolato nel controller.
 	
-	public String getUserAddress() {
-		return userAddress;
-	}
-	public void setUserAddress(String userAddress) {
-		this.userAddress = userAddress;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public String getUserPhone() {
-		return userPhone;
-	}
-	public void setUserPhone(String userPhone) {
-		this.userPhone = userPhone;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserSurname() {
-		return userSurname;
-	}
-	public void setUserSurname(String userSurname) {
-		this.userSurname = userSurname;
-	}
 	public String getGameTitle() {
 		return gameTitle;
 	}
